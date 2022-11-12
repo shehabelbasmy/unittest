448742600a00ea291ef5be2131b2a4cfc9588eb6
shehab <shehabelbasmy>
2022-11-12 18:17:59 +0200

    @displayname

--- starting-project/src/test/java/com/shehab/DemoUtilsTest.java
+++ starting-project/src/test/java/com/shehab/DemoUtilsTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DemoUtilsTest {
 
 	private DemoUtils demoUtils;
@@ -30,13 +34,16 @@ class DemoUtilsTest {
 	static void setupAfterAll() {
 		System.out.println("DemoUtilsTest.setupAfterAll()");
 	}
+	
 	@Test
-	void testEquelAndNotEqual() {
+	@DisplayName("Equal and not equal")
+	void test_Equel_And_Not_Equal() {
 		System.out.println("testEquelAndNotEqual");
 		assertEquals(5,demoUtils.add(2, 3));
 	}
 	
 	@Test
+	@DisplayName("Null and not null")
 	void testNullAndNotNull() {
 		System.out.println("testNullAndNotNull");
 		String str1=null;
