e4deed7afdf4916172fe5ba1e11198f68876c68e
shehab <shehabelbasmy>
2022-11-12 02:04:10 +0200

    section 2 1-7

--- starting-project/src/main/java/com/shehab/DemoUtils.java
+++ starting-project/src/main/java/com/shehab/DemoUtils.java
@@ -1,5 +1,51 @@
 package com.shehab;
 
+import java.util.List;
+
 public class DemoUtils {
 
+	private String academy="Luv2code Academy";
+	
+	private String academyDuplicate=academy;
+	
+	private String [] firstThreeLetterOfAlphabet= {"a","b","c"};
+	
+	private List<String> academyInList=List.of("luv","2","code");
+
+	public String getAcademy() {
+		return academy;
+	}
+
+	public String getAcademyDuplicate() {
+		return academyDuplicate;
+	}
+
+	public String[] getFirstThreeLetterOfAlphabet() {
+		return firstThreeLetterOfAlphabet;
+	}
+
+	public List<String> getAcademyInList() {
+		return academyInList;
+	}
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int multiply(int a,int b) {
+		return a*b;
+	}
+	
+	public Object checkNull(Object obj) {
+		if (obj!=null) {
+			return obj;
+		}
+		return null;
+	}
+	public Boolean isGreater(int n1,int n2) {
+		if (n1>n2) {
+			return true;
+		}
+		return false;
+	}
 }
--- starting-project/src/test/java/com/shehab/DemoUtilsTest.java
+++ starting-project/src/test/java/com/shehab/DemoUtilsTest.java
@@ -1,5 +1,48 @@
 package com.shehab;
 
-public class DemoUtilsTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DemoUtilsTest {
+
+	private DemoUtils demoUtils;
+	
+	@BeforeEach
+	void setupBeforeEach() {
+		System.err.println("Before Each");
+		demoUtils=new DemoUtils();
+	}
+	@AfterEach
+	void setupAfterEach() {
+		System.err.println("After Each");
+	}
+	
+	@BeforeAll
+	static void setupBeforAll() {
+		System.out.println("DemoUtilsTest.setupBeforAll()");
+	}
+	@AfterAll
+	static void setupAfterAll() {
+		System.out.println("DemoUtilsTest.setupAfterAll()");
+	}
+	@Test
+	void testEquelAndNotEqual() {
+		System.out.println("testEquelAndNotEqual");
+		assertEquals(5,demoUtils.add(2, 3));
+	}
+	
+	@Test
+	void testNullAndNotNull() {
+		System.out.println("testNullAndNotNull");
+		String str1=null;
+		String str2="luv2code";
+		assertNull(demoUtils.checkNull(str1),"This String Must Be Null");
+		assertNotNull(demoUtils.checkNull(str2),"This String Must Be Not Null");
+	}
+	
 }
